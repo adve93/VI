a3bb95eb1e01621f537bcfa880c9fad278e19079
Afonso Soares <54285247+adve93@users.noreply.github.com>
2023-10-14 15:23:56 +0100

    ~Finished scatter plot

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ var globalData;
 const margin = {
     top: 20,
     right: 20,
-    bottom: 50,
-    left: 80,
+    bottom: 40,
+    left: 40,
 };
 const width = 600 - margin.left - margin.right;
 const height = 400 - margin.top - margin.bottom;
@@ -16,6 +16,12 @@ function startDashboard() {
 
     d3.csv("pokemon_data.csv")
     .then((data) => {
+
+        data.forEach((d) => {
+            d.attack = +d.attack;
+            d.speed = +d.speed;
+        });
+        
         globalData = data;
 
         //Create functions
