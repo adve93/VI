417497b7e19ff2e188c986eb5f10354648390799
Afonso Soares <54285247+adve93@users.noreply.github.com>
2023-10-27 19:14:37 +0100

    ZOOM

--- create.js
+++ create.js
@@ -122,10 +122,7 @@ function createBubbleChart(data) {
     //Create zoom behaviour
     const zoom = d3.zoom()
         .scaleExtent([0.5, 4])
-        .on("zoom", zoomedBubbleChart)
-        .filter(event => {
-            return !event.ctrlKey; // Disable zoom when the Ctrl key is pressed to allow panning
-        });;
+        .on("zoom", zoomedBubbleChart);
     
     //Attach the zoom behavior to svg
     svg.call(zoom);
